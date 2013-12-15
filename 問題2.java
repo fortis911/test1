5c95941e826ae9365f0b8d9a95bca3a6075e8c49
右田　大輔 <dune116911@yahoo.co.jp>
2013-12-16 02:07:55 +0900

    問一の問二の全文を追加

---
+++ 問題2.java
@@ -0,0 +1,20 @@
+package test1;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class 問題2 extends Applet{
+	public void paint(Graphics g){
+		int dice; /*サイコロの目*/
+		int sum; /*目の合計*/
+		int count; /*サイコロを振った回数*/
+		
+		sum=0;
+		count=0;
+		
+		while 
+		
+		
+	}
+
+}
